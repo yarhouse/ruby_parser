8e6b367ad0a69f62d507bab9580a32413369e3d2
Aaron Yarhouse <lopac1029@gmail.com>
2012-05-24 16:50:54 -0400

    Fixed issue with multiparts

--- parser.rb
+++ parser.rb
@@ -6,27 +6,28 @@ text = lines.join
 code = text.scan(/^--(.+)(\n)(Content-Type: text\/plain)/).join
 boundary = '--' + code.gsub(/\nContent-Type: text\/plain/, "")
 
-# Parse the direction information
-date = "Date: " + text.scan(/(?<=^Date: )(.+)(\n)/).join.to_s
-to = "To: " + text.scan(/(?<=^To: )(.+)(\n)/).join.to_s
-from = "From: " + text.scan(/(?<=^From: )(.+)(\n)/).join.to_s
-cc = "Cc: " + text.scan(/(?<=^Cc: )(.+)(\n)/).join.to_s
-bcc = "Bcc: " + text.scan(/(?<=^Bcc: )(.+)(\n)/).join.to_s
-
 # Split the sections by the boundary and get relevant/readable
 sections = text.split(boundary)
+header = sections[0].gsub(/\n\s+/, " ")
 content = sections[1].gsub(/(^C)(.+)/, "").strip.chomp('--')
+
+# Parse the head information
+date = header.scan(/(^Date: )(.+)(\n)/).join.to_s
+to = header.scan(/(^To: )(.+)(\n)/).join.to_s
+from = header.scan(/(^From: )(.+)(\n)/).join.to_s
+cc = header.scan(/(^Cc: )(.+)(\n)/).join.to_s
+bcc = header.scan(/(^Bcc: )(.+)(\n)/).join.to_s
+subject = header.scan(/(^Subject: )(.+)(\n)/).join.to_s
+top = date + from + to + cc + bcc + subject
 
 # Format
 puts "\n"
-puts "----------------------------------"
+puts "__________________________________"
 puts "\n"
-puts date
-puts from
-puts to
-puts cc
-puts bcc
+puts top
 puts "\n"
-puts "----------------------------------"
+puts "__________________________________"
 puts "\n"
 puts content
+puts "\n"
+puts "__________________________________"
